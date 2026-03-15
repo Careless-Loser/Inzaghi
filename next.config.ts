2954fbbea797b6109404b672e6bd67721cf3a0b3
ifrah <ifrah.imtiaz2612@gmail.com>
2026-03-15 05:59:17 +0400

    chore: add github deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // This is the magic line for GitHub Pages
+  images: {
+    unoptimized: true,   // Required because GitHub can't resize images on the fly
+  },
+  // If your URL is https://careless-loser.github.io/Inzaghi/
+  // basepath: '/Inzaghi', 
 };
 
 export default nextConfig;
